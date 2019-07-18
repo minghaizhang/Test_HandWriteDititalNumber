2a081a419490d37beb43e345e5a037f738acb86d
minghaizhang <1511218612@qq.com>
2019-07-18 10:10:48 +0800

    minghaizhang

---
+++ MNIST_data/train.py
@@ -0,0 +1,101 @@
+#coding:utf-8
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+#number  1 to 10 data
+mnist = input_data.read_data_sets('./MNIST_data/',one_hot=True)
+
+
+
+def compute_accuracy(v_xs,v_ys):
+    global prediction
+    y_pre = sess.run(prediction,feed_dict={xs:v_xs})
+    correct_prediction = tf.equal(tf.argmax(y_pre,1),tf.argmax(v_ys,1))# 行向量对应0或1相同则返回[True,False]
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))# 求True占总数的百分比
+    result = sess.run(accuracy,feed_dict={xs:v_xs,ys:v_ys,keep_prob:1})
+    return result
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape,stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1,shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x,W):
+    # stride [1,x_movement,y_movement,1]
+    # Must have strides[0] = stride[4]=1
+    return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding='SAME')
+
+def max_pool_2x2(x):
+    # stride [1,x_movement,y_movement,1]
+    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+with tf.name_scope('input'):
+    xs = tf.placeholder(tf.float32,[None,784],name='x_input') #28*28
+    ys = tf.placeholder(tf.float32,[None,10],name='y_input') #10 output
+    keep_prob = tf.placeholder(tf.float32)
+    x_image = tf.reshape(xs,[-1,28,28,1])
+#print x_image.shape #[n_samples,28,28,1]
+
+## conv1 layer1
+with tf.name_scope('layer1'):
+    W_conv1 = weight_variable([5,5,1,32],name='W_conv1') #patch 5x5 in size 1, out size 32
+    b_conv1 = bias_variable([32],name='b_conv1')
+    h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1)+b_conv1,name='h_conv1') #output size 28x28x32
+    h_pool1 = max_pool_2x2(h_conv1)       #output size 14x14x32
+
+## conv1 layer2
+with tf.name_scope('layer2'):
+    W_conv2 = weight_variable([5,5,32,64],name='W_conv2') #patch 5x5 in size 32, out size 64
+    b_conv2 = bias_variable([64],name='b_conv2')
+    h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2)+b_conv2,name='h_conv2') #output size 14x14x64
+    h_pool2 = max_pool_2x2(h_conv2)       #output size 7x7x64
+
+## func1 layer ##
+with tf.name_scope('layer3'):
+    W_fc1 = weight_variable([7*7*64,1024],name='W_fc1')
+    b_fc1 = bias_variable([1024],name='b_fc1')
+
+    h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64]) #[n_samples,7,7,64] ->> [n_samples,7*7*64]
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1,name='h_fc1')
+    h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
+
+## func2 layer ##
+with tf.name_scope('layer4'):
+    W_fc2 = weight_variable([1024,10],name='W_fc2')
+    b_fc2 = bias_variable([10],name='b_fc2')
+
+
+prediction = tf.nn.softmax(tf.matmul(h_fc1_drop,W_fc2)+b_fc2)
+tf.summary.histogram('outputs', prediction)
+
+
+#the error between prediction and real data
+with tf.name_scope('loss'):
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction),reduction_indices=[1])) #loss
+    tf.summary.scalar('loss', cross_entropy)
+
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+saver = tf.train.Saver()
+sess = tf.Session()
+merged = tf.summary.merge_all()
+writer = tf.summary.FileWriter('./logs',sess.graph)
+init = tf.global_variables_initializer()
+sess.run(init)
+
+
+
+for i in range(5000):
+    batch_xs,batch_ys = mnist.train.next_batch(100)
+
+    sess.run(train_step,feed_dict={xs:batch_xs,ys:batch_ys,keep_prob:0.5})
+    if i%50 == 0:
+        save_path = saver.save(sess, '/save_data/save_net.ckpt')
+        result = sess.run(merged, feed_dict={xs:batch_xs, ys: batch_ys})
+        writer.add_summary(result, i)
+        print sess.run(cross_entropy, feed_dict={xs:batch_xs, ys:batch_ys})
+        print 'Save to path:', save_path
+        print compute_accuracy(mnist.test.images,mnist.test.labels)
